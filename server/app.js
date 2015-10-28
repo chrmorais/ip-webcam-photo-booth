24729442c4f046c48c9f1f52e904c37033f44428
Nathan Reynolds <email@nreynolds.co.uk>
2015-10-28 18:49:15 +0000

    Revert "Replace arduino_button with trinket_button, a keyboard-emulating button"
    This reverts commit 95f2aa6f5da9c24a2f3c204b3e8265e52d5a6bae.

--- server/app.js
+++ server/app.js
@@ -1,15 +1,18 @@
-var express = require('express');
+var express = require('express.io');
+var fs = require('fs');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var SerialPort = require('serialport').SerialPort;
 
 var indexRoutes = require('./routes/index');
 var previewRoutes = require('./routes/preview');
 var photoRoutes = require('./routes/photo');
 
 var app = express();
+app.http().io();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -26,6 +29,24 @@ app.use('/photo', photoRoutes);
 
 app.set('IP_CAMERA_URL', 'http://192.168.42.129:8080');
 app.set('PHOTO_PATH', __dirname + '/photos');
+
+app.set('BUTTON_SERIAL_PORT', '/dev/cu.usbmodemfa131');
+
+if (app.get('BUTTON_SERIAL_PORT')) {
+    var serialPort = new SerialPort(app.get('BUTTON_SERIAL_PORT'));
+
+    serialPort.on('data', function(data) {
+        if (data.toString() === 'b') {
+            app.io.broadcast('button:press');
+        }
+    });
+
+    app.io.route('led', {
+        set: function(req) {
+            serialPort.write(req.data ? '1' : '0');
+        }
+    });
+}
 
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
